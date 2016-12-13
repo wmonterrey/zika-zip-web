56014421b56bd0e010e325a98d74b3d1229a33ac
William Aviles Monterrey <waviles@icsnicaragua.org>
2016-12-13 09:15:03 -0600

    Pagina de inicio

--- src/main/java/ni/org/ics/zip/HomeController.java
+++ src/main/java/ni/org/ics/zip/HomeController.java
@@ -1,27 +1,50 @@
 package ni.org.ics.zip;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
+import ni.org.ics.zip.service.DashboardService;
 import ni.org.ics.zip.service.UsuarioService;
+import ni.org.ics.zip.users.model.UserSistema;
+
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/*")
 public class HomeController {
+    @Resource(name="dashboardService")
+	private DashboardService dashboardService;
 	@Resource(name="usuarioService")
 	private UsuarioService usuarioService;
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Model model) {
-    	logger.info("Pagina principal");
+        logger.info("Estudio ZIP Iniciada");
+        UserSistema usuario = usuarioService.getUser(SecurityContextHolder.getContext().getAuthentication().getName());
+        DateTime dt2 = new DateTime();
+        DateTime dt1 = dt2.minusDays(30);
+        long d1 =dt1.getMillis();
+        long d2 =dt2.getMillis();
+        List<Object> tamizajesDia = dashboardService.tamizajesDia(usuario,d1,d2);
+        model.addAttribute("tamizajesDia", tamizajesDia);
+        model.addAttribute("usuario", usuario);
         return "home";
     }
     
@@ -56,5 +79,24 @@ public class HomeController {
 	{	
 		return "OK";
 	}
+    
+    /**
+     * Acepta una solicitud GET para JSON
+     * @return Un arreglo JSON
+	 * @throws ParseException 
+     */
+	@RequestMapping(value = "updateDashboard", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody Map<String, List<Object>> fetchDashboardJson(@RequestParam(value = "start", required = true) String fec1,
+    		@RequestParam(value = "end", required = true) String fec2) throws ParseException {
+        logger.info("Actualizando dashboard");
+        UserSistema usuario = usuarioService.getUser(SecurityContextHolder.getContext().getAuthentication().getName());
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date date1 = formatter.parse(fec1);
+        Date date2 = formatter.parse(fec2);
+        Map<String, List<Object>> mapModel = new HashMap<String, List<Object>>();
+        List<Object> tamizajesDia = dashboardService.tamizajesDia(usuario,date1.getTime(),date2.getTime());
+        mapModel.put("lista1", tamizajesDia);
+        return mapModel;
+    }
     
 }
---
+++ src/main/java/ni/org/ics/zip/service/DashboardService.java
@@ -0,0 +1,47 @@
+package ni.org.ics.zip.service;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+
+import ni.org.ics.zip.users.model.UserSistema;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("dashboardService")
+@Transactional
+public class DashboardService {
+	
+	@Resource(name="sessionFactory")
+	private SessionFactory sessionFactory;
+	
+	@SuppressWarnings("unchecked")
+	public List<Object> tamizajesDia(UserSistema usuario, long date1, long date2){
+		// Retrieve session from Hibernate
+		Session session = sessionFactory.getCurrentSession();
+		Timestamp timeStampInicio = new Timestamp(date1);
+		Timestamp timeStampFinal = new Timestamp(date2);
+		// Create a Hibernate query (HQL)
+        Query query = session.createQuery("select zp00.scrVisitDate as fecha, "+
+        		"count(zpPre.cs) as total, " +
+        		"sum(case zpPre.cs when 'Socrates Flores' then 1 else 0 end) as CSSFV, " +
+        		"sum(case zpPre.cs when 'Villa Libertad' then 1 else 0 end) as CSVL, " +
+        		"sum(case zpPre.cs when 'Francisco Buitrago' then 1 else 0 end) as CSFB " +
+        		"FROM Zp00Screening zp00 , ZpPreScreening zpPre " +
+        		"where zp00.preScreenId = zpPre.recId and zp00.pasive = '0' and zp00.preScreenId in (select recId from ZpPreScreening zpPre where zpPre.pasive = '0' and zpPre.cs in " +
+        		"(Select uc.centro.cs from UserCenter uc where uc.user.username =:usuarioactual and uc.pasive = '0')) "+
+        		"and zp00.scrVisitDate between :fechaInicio and :fechaFinal group by zp00.scrVisitDate order by zp00.scrVisitDate");
+		query.setTimestamp("fechaInicio", timeStampInicio);
+		query.setTimestamp("fechaFinal", timeStampFinal);	
+		query.setParameter("usuarioactual",usuario.getUsername());
+        // Retrieve all
+		return  query.list();
+	}
+	
+}
